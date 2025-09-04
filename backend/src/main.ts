dc15c608ef24a0f9e4eab40060bd6de4621b2ddf
maelghrib <maelghrib@gmail.com>
2025-09-04 11:15:03 +0300

    Added Helmet

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,12 @@ import {NestFactory} from '@nestjs/core';
 import {AppModule} from './app.module';
 import {SwaggerModule, DocumentBuilder, SwaggerDocumentOptions} from '@nestjs/swagger';
 import {ValidationPipe} from "@nestjs/common";
+import helmet from "helmet";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
+    app.use(helmet());
 
     app.useGlobalPipes(
         new ValidationPipe({
