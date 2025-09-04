287045b382bda1c3956ad6a4eb985d5f40ecfe43
maelghrib <maelghrib@gmail.com>
2025-09-04 19:27:33 +0300

    Added home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,45 @@
-import {Button, HStack} from "@chakra-ui/react"
+import * as ChakraUI from "@chakra-ui/react";
+import {cookies} from 'next/headers';
+import {redirect} from 'next/navigation';
+import axios from '@/utils/axios';
+import {APIEndpoint, ResponseStatus, PageRoute} from '@/utils/constants';
+import {LogoutResponse, UserData, UserProfileResponse} from '@/utils/types';
+import {AxiosResponse} from "axios";
+import {LoginButton} from "@/components/LogoutButton";
 
-const Demo = () => {
+export default async function Home() {
+
+    const cookieStore = await cookies();
+    const token = cookieStore.get('accessToken')?.value;
+
+    if (!token) {
+        redirect(PageRoute.LOGIN);
+    }
+
+    let user: UserData | null = null;
+    try {
+        const response: AxiosResponse<UserProfileResponse> = await axios.get<UserProfileResponse>(
+            APIEndpoint.PROFILE, {headers: {Authorization: `Bearer ${token}`}}
+        );
+
+        if (response.data.status === ResponseStatus.SUCCESS) {
+            user = response.data.user!!;
+        } else {
+            redirect(PageRoute.LOGIN);
+        }
+    } catch (err) {
+        console.error(err);
+        redirect(PageRoute.LOGIN);
+    }
+
+    const hello_user_text = `Hello ${user?.name ?? ''}!`;
+    const welcome_text = "Welcome to Easygenerator";
+
     return (
-        <HStack>
-            <Button>Click me</Button>
-            <Button>Click me</Button>
-        </HStack>
-    )
-}
-
-export default function Home() {
-    return <Demo/>;
+        <ChakraUI.Center w="100%" h="100vh" flexDir="column" gap={"5"}>
+            <ChakraUI.Text fontSize="40px" fontWeight="bold">{hello_user_text}</ChakraUI.Text>
+            <ChakraUI.Text fontSize="30px" fontWeight="bold">{welcome_text}</ChakraUI.Text>
+            <LoginButton />
+        </ChakraUI.Center>
+    );
 }
